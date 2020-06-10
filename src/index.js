ba0616a4c2c9ed653e1b1fb1850dc814cbace1c5
twic69 <gaming.from.integral.2k18@gmail.com>
2020-06-10 16:12:38 +0700

    react-todo-list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const header = (
+  <h1>header</h1>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Todo = () => {
+  return (
+    <div className="col-10">
+          <h2>h2</h2>
+          <input type="text" size="10"/>
+          <button>Add</button><br/>
+          <input type="radio"/><label htmlFor="in1">radio_button_#1</label><br/>
+          <input type="radio"/><label htmlFor="in2">radio_button_#2</label><br/>
+          <input type="radio"/><label htmlFor="in3">radio_button_#3</label><br/>
+    </div>
+  )
+}
+
+const List = () => {
+  return (
+      <div className="col-2">
+          <h2>h2</h2>
+          <input type="text" size="10"/><br/>
+          <input type="checkbox"/><label htmlFor="in1">checkbox_button_#1</label><br/>
+          <input type="checkbox"/><label htmlFor="in2">checkbox_button_#2</label><br/>
+          <input type="checkbox"/><label htmlFor="in3">checkbox_button_#3</label><br/>
+      </div>
+  );
+}
+
+const App = () => {
+  return (
+      <div className="container">
+         <div className="row justify-content-center">{header}</div>
+          <div className="row"><Todo/><List/></div>
+      </div>
+  )
+}
+
+ReactDOM.render(<App/> , document.getElementById("root"));
